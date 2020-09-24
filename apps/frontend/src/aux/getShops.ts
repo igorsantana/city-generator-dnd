27c38a67b331433583122a28e51117198f97c4a9
Igor Santana <igorpsant95@gmail.com>
2020-09-24 20:00:22 -0300

    shops

--- apps/frontend/src/aux/getShops.ts
+++ apps/frontend/src/aux/getShops.ts
@@ -1,28 +1,116 @@
 function getShops() {
-  return [{
-
-  }];
+  return [
+    {
+      name: 'Pawnshop',
+      value: 'pawnshop',
+      generate: true,
+    },
+    {
+      name: 'Smithy',
+      value: 'smithy',
+      generate: true,
+    },
+    {
+      name: 'Herbs/incense',
+      value: 'herbsincense',
+      generate: true,
+    },
+    {
+      name: 'Carpenter',
+      value: 'carpenter',
+      generate: true,
+    },
+    {
+      name: 'Fruits/vegetables',
+      value: 'fruitsvegetables',
+      generate: true,
+    },
+    {
+      name: 'Weaver',
+      value: 'weaver',
+      generate: true,
+    },
+    {
+      name: 'Dried Meats',
+      value: 'driedmeats',
+      generate: true,
+    },
+    {
+      name: 'Jeweler',
+      value: 'jeweler',
+      generate: true,
+    },
+    {
+      name: 'Pottery',
+      value: 'pottery',
+      generate: true,
+    },
+    {
+      name: 'Baker',
+      value: 'baker',
+      generate: true,
+    },
+    {
+      name: 'Undertaker',
+      value: 'undertaker',
+      generate: true,
+    },
+    {
+      name: 'Mapmaker',
+      value: 'mapmaker',
+      generate: true,
+    },
+    {
+      name: 'Bookstore',
+      value: 'bookstore',
+      generate: true,
+    },
+    {
+      name: 'Tailor',
+      value: 'tailor',
+      generate: true,
+    },
+    {
+      name: 'Moneylender',
+      value: 'moneylender',
+      generate: true,
+    },
+    {
+      name: 'Ropemaker',
+      value: 'ropemaker',
+      generate: true,
+    },
+    {
+      name: 'Weapons/armor',
+      value: 'weaponsarmor',
+      generate: true,
+    },
+    {
+      name: 'Mason',
+      value: 'mason',
+      generate: true,
+    },
+    {
+      name: 'Chandler',
+      value: 'chandler',
+      generate: true,
+    },
+    {
+      name: 'Scribe',
+      value: 'scribe',
+      generate: true,
+    },
+    {
+      name: 'Tavern',
+      value: 'tavern',
+      generate: true,
+    },
+    {
+      name: 'Magic Shop',
+      value: 'magicshop',
+      generate: true,
+    },
+  ];
 }
 
 export { getShops };
-
-// - Pawnshop
-// - Smithy
-// - Herbs/incense
-// - Carpenter
-// - Fruits/vegetables
-// - Weaver
-// - Dried meats
-// - Jeweler
-// - Pottery
-// - Baker
-// - Undertaker
-// - Mapmaker
-// - Books
-// - Tailor
-// - Moneylender
-// - Ropemaker
-// - Weaponsfarmor
-// - Mason
-// - Chandler
-// - Scribe
